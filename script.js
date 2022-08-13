eae831a1b5583c5f3d2315089d9c5a636f90ccaa
Pavlukan <theremustbesomethingsmart@gmail.com>
2022-08-13 10:51:02 +0300

    Parse the variables num1 and num2 and return floating point number

--- script.js
+++ script.js
@@ -15,19 +15,19 @@ let waitingForSecondNumber = false;
 let isDecimal = false;
 
 function add(num1, num2) {
-    return num1 + num2;
+    return parseFloat(num1) + parseFloat(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return parseFloat(num1) - parseFloat(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return parseFloat(num1) * parseFloat(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return parseFloat(num1) / parseFloat(num2);
 }
 
 function operate(num1, operator, num2) {
@@ -73,7 +73,8 @@ function clearDisplay() {
     previousDisplayValue.textContent = "";
     num1 = "";
     num2 = "";
-    operator = null;
+    operator = "";
+    waitingForSecondNumber = false;
 }
 
 function deleteCharacter() {
